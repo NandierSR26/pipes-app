0506308f79942e4ecd8a4f3ec1b0c4e1516669d5
Nandier Ruiz <nandier.ruiz@gmail.com>
2024-02-06 14:19:00 -0500

    feat: :art: Change Menu component to MenuBar

--- src/app/shared/components/menu/menu.component.ts
+++ src/app/shared/components/menu/menu.component.ts
@@ -14,14 +14,35 @@ export class MenuComponent implements OnInit {
   ngOnInit() {
     this.menuItems = [
       {
-        label: 'New',
-        icon: 'pi pi-fw pi-plus',
+        label: 'Pipes de Angular',
+        icon: 'pi pi-desktop',
+        items: [
+          {
+            label: 'Textos y Fechas',
+            icon: 'pi pi-align-left'
+          },
+          {
+            label: 'Numeros',
+            icon: 'pi pi-dollar'
+          },
+          {
+            label: 'No Comunes',
+            icon: 'pi pi-globe'
+          }
+        ]
       },
       {
-        label: 'Delete',
-        icon: 'pi pi-fw pi-trash'
+        label: 'Pipes Personalizados',
+        icon: 'pi pi-cog',
+        items: [
+          {
+            label: 'Otro Elemento',
+            icon: 'pi pi-cog'
+          }
+        ]
       }
     ];
+
   }
   
 }
