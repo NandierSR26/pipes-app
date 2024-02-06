579f3614437aae134006688a2e2f9a84a45c6405
Nandier Ruiz <nandier.ruiz@gmail.com>
2024-02-06 14:04:28 -0500

    feat: :construction: Shared-menu component implemented, installation of Angular animations dependency

--- src/app/shared/components/menu/menu.component.ts
+++ src/app/shared/components/menu/menu.component.ts
@@ -1,11 +1,27 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { MenuItem } from 'primeng/api';
 
 @Component({
-  selector: 'app-menu',
+  selector: 'shared-menu',
   templateUrl: './menu.component.html',
   styles: [
   ]
 })
-export class MenuComponent {
+export class MenuComponent implements OnInit {
 
+  public menuItems: MenuItem[] | undefined;
+
+  ngOnInit() {
+    this.menuItems = [
+      {
+        label: 'New',
+        icon: 'pi pi-fw pi-plus',
+      },
+      {
+        label: 'Delete',
+        icon: 'pi pi-fw pi-trash'
+      }
+    ];
+  }
+  
 }
