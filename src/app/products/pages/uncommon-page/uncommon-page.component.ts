73c6d56ae28cf4e03d09f97830483908fcf1e86f
Nandier Ruiz <nandier.ruiz@gmail.com>
2024-02-08 10:50:01 -0500

    feat: :fire: Implementation of the i18nSelect pipe completed

--- src/app/products/pages/uncommon-page/uncommon-page.component.ts
+++ src/app/products/pages/uncommon-page/uncommon-page.component.ts
@@ -8,4 +8,17 @@ import { Component } from '@angular/core';
 })
 export class UncommonPageComponent {
 
+  // i18nSelect
+  public name: string = 'Nandier';
+  public gender: 'male' | 'female' = 'male';
+  public invitationMap = {
+    'male': 'invitarlo',
+    'female': 'invitarla'
+  }
+
+  changeClient(): void {
+    this.name = 'Hinata';
+    this.gender = 'female'
+  }
+
 }
