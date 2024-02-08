d18e3709b6524bb6c65b7e4b3b25baf595b3be79
Nandier Ruiz <nandier.ruiz@gmail.com>
2024-02-08 10:36:04 -0500

    fix: :wrench: Fixed uncommon component name

--- src/app/products/pages/uncommon-page/uncommon-page.component.ts
+++ src/app/products/pages/uncommon-page/uncommon-page.component.ts
@@ -2,7 +2,7 @@ import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-common-page',
-  templateUrl: './common-page.component.html',
+  templateUrl: './uncommon-page.component.html',
   styles: [
   ]
 })
--- src/app/products/products.module.ts
+++ src/app/products/products.module.ts
@@ -4,7 +4,7 @@ import { CommonModule } from '@angular/common';
 import { ProductsRoutingModule } from './products-routing.module';
 import { BasicsPageComponent } from './pages/basics-page/basics-page.component';
 import { NumbersPageComponent } from './pages/numbers-page/numbers-page.component';
-import { UncommonPageComponent } from './pages/uncommon-page/common-page.component';
+import { UncommonPageComponent } from './pages/uncommon-page/uncommon-page.component';
 import { PrimeNgModule } from '../prime-ng/prime-ng.module';
 
 
